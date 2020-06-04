77bf9d81ecdb98603ead466408085996d1b24986
samaprk <65785669+samaprk@users.noreply.github.com>
2020-06-05 00:05:40 +0530

    action

---
+++ home/assets/js/action.js
@@ -0,0 +1,9 @@
+var logged=1;
+if(logged == 1){
+    $(".logged").css("display","block");
+    $(".not-logged").css("display","none");
+}
+else{
+    $(".logged").css("display","none");
+    $(".not-logged").css("display","block");
+}
